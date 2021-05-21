03afd22fb2b4f710aaee9cd33794cf01a0b2a646
AntonioDelCid <antonio.delcid@camasfacenco.net>
2021-05-20 22:12:45 -0600

    cambios realizados

--- StreamingWebApi/Startup.cs
+++ StreamingWebApi/Startup.cs
@@ -55,7 +55,10 @@ namespace StreamingWebApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseCors("Todos");
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
             app.UseHttpsRedirection();
             app.UseMvc();
         }
